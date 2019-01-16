d4d081e914f7ac9bd75e67f8b2f2884e34bdd5dc
tschatzmann <tarrta2002@gmail.com>
2019-01-15 21:05:18 -0800

    file for project

---
+++ assets/javascript/gif.js
@@ -0,0 +1,76 @@
+
+  destinationContainer = document.getElementById("destinations-view")
+  // Initial array of destinations
+  var destinations = ["London Tower", "Irland", "Switzerland", "New Zeland"];
+
+  // displaydestinationInfo function re-renders the HTML to display the appropriate content
+  function displaydestinationInfo() {
+
+    var searchWord = $(this).attr("data-name");
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchWord}&api_key=dc6zaTOxFJmzC&limit=1`;
+    console.log(searchWord);
+    console.log(queryURL);
+$.ajax({
+url: queryURL,
+method: "GET"
+}).then(function (response) {
+console.log(response);
+console.log(response.data[0].url);
+var results = response.data[0];
+// Only taking action if the photo has an appropriate rating
+if (results.rating !== "r" && results.rating !== "pg-13") {
+    // Creating a div template gif
+    var ptemplate = `<p>
+        ${results.ratings}</p>
+        <div>
+        <img src="${ results.images.fixed_height.url }">
+        </div>`
+        console.log(ptemplate);
+        ddestinationContainer.innerHTML = ptemplate;
+
+}
+})
+  };
+
+
+  function renderButtons() {
+
+    // Deleting the destinations prior to adding new destinations
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of destinations
+    for (var i = 0; i < destinations.length; i++) {
+
+      // Then dynamicaly generating buttons for each destination in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adding a class of destination-btn to our button
+      a.addClass("destination-btn");
+      // Adding a data-attribute
+      a.attr("data-name", destinations[i]);
+      // Providing the initial button text
+      a.text(destinations[i]);
+      // Adding the button to the buttons-view div
+      $("#buttons-view").append(a);
+    }
+  };
+
+  // This function handles events where a destination button is clicked
+  $("#add-destination").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var destination = $("#destination-input").val().trim();
+
+    // Adding destination from the textbox to our array
+    destinations.push(destination);
+
+    // Calling renderButtons which handles the processing of our destination array
+    renderButtons();
+  });
+
+  // Adding a click event listener to all elements with a class of "destination-btn"
+  $(document).on("click", ".destination-btn", displaydestinationInfo);
+
+  // Calling the renderButtons function to display the intial buttons
+  renderButtons();
